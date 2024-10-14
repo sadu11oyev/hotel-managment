fa531c47abef5179f002bed96ee60f4aab168dd6
Бахтиёр Садуллоев <baxtisadulloyev@gmail.com>
2024-10-14 14:28:49 +0500

    first commit

---
+++ src/test/java/baxtiyor/hotel/hotelmanagment/HotelManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package baxtiyor.hotel.hotelmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
