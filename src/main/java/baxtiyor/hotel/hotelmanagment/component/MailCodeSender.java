7baf2d2d5a37b0c153f9b41dd94ef8f401328292
Бахтиёр Садуллоев <baxtisadulloyev@gmail.com>
2024-10-15 15:51:54 +0500

    security config added 2

--- src/main/java/baxtiyor/hotel/hotelmanagment/component/MailCodeSender.java
+++ src/main/java/baxtiyor/hotel/hotelmanagment/component/MailCodeSender.java
@@ -1,8 +1,10 @@
 package baxtiyor.hotel.hotelmanagment.component;
 
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +13,28 @@ import org.springframework.stereotype.Service;
 public class MailCodeSender {
     private final JavaMailSender javaMailSender;
 
+//    @Async
+//    public void sendMessage(Integer code, String email){
+//        SimpleMailMessage mailMessage=new SimpleMailMessage();
+//        mailMessage.setSubject("Auth Code");
+//        mailMessage.setText("Your verification code is :"+code);
+//        mailMessage.setTo(email);
+//        javaMailSender.send(mailMessage);
+//    }
     @Async
-    public void sendMessage(Integer code, String email){
-        SimpleMailMessage mailMessage=new SimpleMailMessage();
-        mailMessage.setSubject("Auth Code");
-        mailMessage.setText("Your verification code is :"+code);
-        mailMessage.setTo(email);
-        javaMailSender.send(mailMessage);
+    public void sendMessage(Integer code, String recipient) {
+        try {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setFrom("baxtisadulloyev@gmail.com");
+            helper.setTo(recipient);
+            helper.setSubject("Auth Code");
+            helper.setText("Your verification code is :" + code, true);
+
+            javaMailSender.send(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
